782f34e8973a958dd3c46709994d7ff26565cc08
Evangelinavalle <evangelinavalle8@gmail.com>
2025-01-22 16:46:34 -0500

    pushing react

--- react_1/src/App.js
+++ react_1/src/App.js
@@ -1,24 +1,22 @@
+// App.js
 import "./index.css";
 import Card from "./Components/Card";
-
-import { useState} from "react";
+import { useState } from "react";
 
 export default function App() {
+  const [upvote, setupvote] = useState(0);
 
-let [upvote, setupvote] = useState(0);
-
-function like () {
-return setupvote((upvote += 1));
-}
-
+  function like() {
+    setupvote((prevUpvote) => prevUpvote + 1); // Using the functional update pattern
+  }
 
   return (
     <div className="App">
       {upvote}
-      <button>🐤</button>
-      <Card Name="Evan" Work="SWE" />
-      <Card Name="Korina" Work="Chef" pics/> 
-      <Card Name="Sam" Work="Hunter" />
+      <button onClick={like}>🐤</button>
+      <Card Name="Evan" Work="SWE" pics="https://miro.medium.com/v2/resize:fit:1200/1*rKl56ixsC55cMAsO2aQhGQ@2x.jpeg" />
+      <Card Name="Korina" Work="Chef" pics="https://miro.medium.com/v2/resize:fit:1200/1*rKl56ixsC55cMAsO2aQhGQ@2x.jpeg" /> 
+      <Card Name="Sam" Work="Hunter" pics="https://miro.medium.com/v2/resize:fit:1200/1*rKl56ixsC55cMAsO2aQhGQ@2x.jpeg" />
     </div>
   );
 }
--- react_1/src/Components/Card.js
+++ react_1/src/Components/Card.js
@@ -1,11 +1,12 @@
 import "./Card.css";
 
-function Card({ Name, Work }) {
+// Card.js
+function Card({ Name, Work, pics }) {
   return (
     <div className="card">
       <img
-        src="https://miro.medium.com/v2/resize:fit:1200/1*rKl56ixsC55cMAsO2aQhGQ@2x.jpeg"
-        alt="Profile"
+        src={pics || "https://miro.medium.com/v2/resize:fit:1200/1*rKl56ixsC55cMAsO2aQhGQ@2x.jpeg"}
+        alt={`${Name}'s Profile`}
         width="200px"
       />
       <h1>{Name}</h1>
@@ -13,6 +14,5 @@ function Card({ Name, Work }) {
     </div>
   );
 }
-//this is just see if the folders and my work is pushing to git hub
 
 export default Card;
